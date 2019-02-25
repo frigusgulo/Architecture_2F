3cda570604fe24cfa323bc92197548447536823b
cg117634 <35784792+cg117634@users.noreply.github.com>
2019-02-24 20:57:28 -0700

    more adjustments and added missing colons

--- Assembler/cinstruct.py
+++ Assembler/cinstruct.py
@@ -30,69 +30,69 @@ class Assembler(object):
 
 def _init_(self,file):
 	for line in open(file, 'r'):
-        if not line.strip():
-            self.assemblycode.append(line.replace(" ",""))
+		if not line.strip():
+			self.assemblycode.append(line.replace(" ",""))
 
 def assemble(self):
-    for line in self.assemblycode:
-        if not line.startswith("//")
-            gen_code_line_addr(line.split("//")[0])
-            gen_code(line.split("//")[0])
+	for line in self.assemblycode:
+		if not line.startswith("//"):
+			gen_code_line_addr(line.split("//")[0])
+			gen_code(line.split("//")[0])
 
 
 
 def gen_code_line_addr(line):
-    global code_addr
-    global prognextadd
+	global code_addr
+	global prognextadd
 
-    newline = re.split('(\(|\)|=|;|@)', line)
-    if newline[1] == '(':
-        if newline[2].isdigit():
-            return
-        if newline[2] not in code_addr
-            code_addr[newline[2]] = prognextadd
+	newline = re.split('(\(|\)|=|;|@)', line)
+	if newline[1] == '(':
+		if newline[2].isdigit():
+			return
+		if newline[2] not in code_addr:
+			code_addr[newline[2]] = prognextadd
 
-    prognextadd += 1
+	prognextadd += 1
 
 def gen_code(line):
 
-    global memnextadd
-    global code_addr
+	global memnextadd
+	global code_addr
 
-    newline = re.split('(\(|\)|=|;|@)', line)
-    if newline[1] == '(':
-        return
-    if newline[1] == '=':
-        if newline[2] in comp_a:
-            a = '1'
-            print('111' + a + comp_a[newline[2]] + dest[newline[0]] + '000')
-        else :
-            a = '0'
-            print('111' + a + comp_nota[newline[2]] + dest[newline[0]] + '000')
-        return
-    if newline[1] == ';':
-        print('111' + '0' + comp_a[newline[0]] + '000' + jump[newline[2]])
-        return
-    if newline[1] == '@':
-        if newline[2].isdigit():
-            print('0' + format(int(newline[2]), '015b'))
-            return
-        if newline[2] in code_addr:
-            print('0' + format(code_addr[newline[2]], '015b'))
-            return
-        if newline[2] in def_add:
-            print('0' + format(def_add[newline[2]], '015b'))
-            return
-        if newline[2] in newvars:
-            print('0' + format(newvars[newline[2]], '015b'))
-            return
-        newvars[newline[2]] = memnextadd
-        print('0' + format(memnextadd, '015b'))
-        memnextadd += 1
-        return
-    else:
-        return
+	newline = re.split('(\(|\)|=|;|@)', line)
+	if newline[1] == '(':
+		return
+	if newline[1] == '=':
+		if newline[2] in comp_a:
+			a = '1'
+			print('111' + a + comp_a[newline[2]] + dest[newline[0]] + '000')
+		else :
+			a = '0'
+			print('111' + a + comp_nota[newline[2]] + dest[newline[0]] + '000')
+		return
+	if newline[1] == ';':
+		print('111' + '0' + comp_a[newline[0]] + '000' + jump[newline[2]])
+		return
+	if newline[1] == '@':
+		if newline[2].isdigit():
+			print('0' + format(int(newline[2]), '015b'))
+			return
+		if newline[2] in code_addr:
+			print('0' + format(code_addr[newline[2]], '015b'))
+			return
+		if newline[2] in def_add:
+			print('0' + format(def_add[newline[2]], '015b'))
+			return
+		if newline[2] in newvars:
+			print('0' + format(newvars[newline[2]], '015b'))
+			return
+		newvars[newline[2]] = memnextadd
+		print('0' + format(memnextadd, '015b'))
+		memnextadd += 1
+		return
+	else:
+		return
 
 if __name__ == "__main__":
-    assembler = Assembler(sys.argv[1])
-    assembler.assemble() # run our code
+	assembler = Assembler(sys.argv[1])
+	assembler.assemble() # run our code
