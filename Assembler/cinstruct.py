867f0653cd08e40cf94be7ef098631ed38caa0cd
Franklyn Dunbar <franklyn.dunbar@umontana.edu>
2019-02-24 20:07:09 -0700

    update with imports

--- Assembler/cinstruct.py
+++ Assembler/cinstruct.py
@@ -5,6 +5,9 @@ Dictionary for an assembler for hack.
 
 #!/usr/bin/env python3
 
+import sys
+import re
+
 dest = {"null": "000", "M": "001", "D": "010", "MD": "011","A":"100","AM":"101","AD":"110","AMD":"111" }
 
 jump = {"null":"000","JGT":"001","JEQ":"010","JGE":"011","JLT":"100","JNE":"101","JLE":"110","JMP":"111"}
